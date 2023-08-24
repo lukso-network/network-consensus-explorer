c31c11a6d878adfc3af80fda14bee1e90f597b8f
mxmar <macht4tech@gmail.com>
2023-08-24 12:31:48 +0200

    Changed total supply endpoint calculation
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- handlers/network.go
+++ handlers/network.go
@@ -12,7 +12,7 @@ import (
 // Supply godoc
 // @Summary Get total supply of all native network tokens in existence
 // @Tags Supply
-// @Description Returns information about  total supply of all native network tokens in existence (in wei).
+// @Description Returns information about total supply of all native network tokens in existence (in wei).
 // @Produce  json
 // @Success 200 {object} types.ApiResponse{data=types.SupplyResponse} "Success"
 // @Failure 400 {object} types.ApiResponse "Failure"
@@ -33,30 +33,17 @@ func Supply(w http.ResponseWriter, r *http.Request) {
 		logger.WithError(err).Error("error getting total amount withdrawn from db")
 	}
 
-	//TODO: provide validators or custom GetValidatorsTotalBalance
-	latestBalances, err := db.BigtableClient.GetValidatorBalanceHistory(validators, latestFinalizedEpoch, latestFinalizedEpoch)
+	balanceStatistics, err := db.BigtableClient.GetValidatorBalanceStatistics(latestFinalizedEpoch, latestFinalizedEpoch)
 	if err != nil {
-		logger.Errorf("error getting validator balance data in GetValidatorEarnings: %v", err)
+		logger.WithError(err).Error("error getting validator balance data in GetValidatorBalanceStatistics")
 	}
 
-	balancesMap := make(map[uint64]*types.Validator, 0)
-	totalBalance := uint64(0)
-
-	for balanceIndex, balance := range latestBalances {
-		if len(balance) == 0 {
-			continue
-		}
-
-		if balancesMap[balanceIndex] == nil {
-			balancesMap[balanceIndex] = &types.Validator{}
-		}
-		balancesMap[balanceIndex].Balance = balance[0].Balance
-		balancesMap[balanceIndex].EffectiveBalance = balance[0].EffectiveBalance
-
-		totalBalance += balance[0].Balance
+	totalValidatorsBalance := uint64(0)
+	for _, statistic := range balanceStatistics {
+		totalValidatorsBalance += statistic.EndBalance
 	}
 
-	totalSupply := genesisTotalSupply + totalAmountWithdrawn
+	totalSupply := genesisTotalSupply + totalAmountWithdrawn + totalValidatorsBalance
 
 	data := types.SupplyResponse{
 		TotalSupply: totalSupply,
