588ece904aa1e70a54b5ab3f1d6683735ef7c703
Johann BICH <2253470+kalote@users.noreply.github.com>
2023-09-01 11:15:56 +0200

    comment withdrawals menu item

--- handlers/pageData.go
+++ handlers/pageData.go
@@ -295,14 +295,14 @@ func createMenuItems(active string, isMain bool) []types.MainMenuItem {
 							Icon:  "fa-file-import",
 						},
 					},
-				}, {
-					Links: []types.NavigationLink{
-						{
-							Label: "Withdrawals",
-							Path:  "/validators/withdrawals",
-							Icon:  "fa-money-bill",
-						},
-					},
+				// }, {
+				// 	Links: []types.NavigationLink{
+				// 		{
+				// 			Label: "Withdrawals",
+				// 			Path:  "/validators/withdrawals",
+				// 			Icon:  "fa-money-bill",
+				// 		},
+				// 	},
 				},
 			},
 		},
